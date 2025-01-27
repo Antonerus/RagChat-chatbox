5c03222f5d4c10d82ad28576f6a37de405c35d2c
anton <antony99009@gmail.com>
2025-01-27 11:13:43 -0500

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Provider } from "@/components/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          `${geistSans.variable}`,
+          `${geistMono.variable}`,
+          `antialiased`,
+          "min-h-screen",
+        )}
       >
-        {children}
+        <Provider>
+          <main className="h-screen dark text-foreground bg-background flex items-center justify-center">
+            <div className="grid-rows-1 grid-cols-1 ">
+              <p className="box-border w-[500px] pb-5">Hello! Welcome to this project, where you can send information to a Ragchat AI through website links and start asking questions.</p>
+              <div className="flex w-full items-center rounded-full">
+                <div className="flex-1 border-b border-gray-300"></div>
+                <span className="text-black text-lg font-semibold leading-8 px-8 py-3">How it works</span>
+                <div className="flex-1 border-b border-gray-300"></div>
+              </div>
+              <p className="box-border w-[500px] pb-5">Enter a valid URL at the end of this website's link to get started!</p>
+              <div className="flex w-full items-center rounded-full">
+                <div className="flex-1 border-b border-gray-300"></div>
+                <span className="text-black text-lg font-semibold leading-8 px-8 py-3">Quick Example</span>
+                <div className="flex-1 border-b border-gray-300"></div>
+              </div>
+              <p className="box-border w-[500px]">localhost:3000/https://lotr.fandom.com/wiki/Aragorn_II</p>
+            </div>
+          </main>
+          <main className="h-screen dark text-foreground bg-background">{children}</main>
+        </Provider>
       </body>
     </html>
   );
---
+++ src/components/Message.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils"
+import { Bot, User } from "lucide-react"
+
+interface MessageProps {
+    content: string
+    isUserMessage: boolean
+}
+
+export const Message = ({ content, isUserMessage }: MessageProps) => {
+    return (
+        <div className={cn({
+            "bg-zinc-800": isUserMessage,
+            "bg-zinc-900/25": !isUserMessage,
+        })}>
+            <div className="p-6">
+                <div className="max-w-3xl mx-auto flex items-start gap-2.5">
+                    <div className={cn("size-10 shrink-0 aspect-square rounded-full border border-zinc-700 bg-zinc-900 flex justify-center items-center", 
+                    {
+                        "bg-blue-950 border-blue-700 text-zinc-200": isUserMessage,
+                    })}>
+                        {isUserMessage ? <User className="size-5" /> : <Bot className="size-5 text-white" />}
+                    </div>
+                    <div className="flex flex-col ml-6 w-full">
+                        <div className="flex items-center space-x-2">
+                            <span className={cn(
+                                "text-sm font-semibold",
+                                {
+                                    "text-gray-200": isUserMessage,
+                                    "text-gray-400": !isUserMessage,
+                                }
+                            )}>
+                                {isUserMessage ? "You" : "Website"}
+                            </span>
+                        </div>
+                        <p className={cn(
+                                "text-sm font-normal py-2.5",
+                                {
+                                    "text-gray-200": isUserMessage, // Adjusted for better visibility
+                                    "text-gray-400": !isUserMessage, // Softer contrast for bot messages
+                                }
+                            )}>{content}</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
